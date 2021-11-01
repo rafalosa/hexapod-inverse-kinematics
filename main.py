d8f25b12db116834f17f8b7c570d26c548b01876
rafalosa <rafal.osad@gmail.com>
2021-11-01 11:49:16 +0100

    Cleanup.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import simulators
+from robot import Hexapod, Core
 
 
 if __name__ == '__main__':
-    print("Hello")
+
+    fig = plt.figure(figsize=(10, 12))
+    ax = fig.add_subplot(111, projection="3d")
+
+    body = Core(ax, 20, 30, 20)
+    bot = Hexapod(ax, body)
+    bot.add_leg(30, 40)
+    bot.add_leg(30, 40)
+    bot.add_leg(30, 40)
+    bot.add_leg(30, 40)
+    bot.add_leg(30, 40)
+    bot.add_leg(30, 40)
+
+    sim = simulators.ForwardKinematicsPreview(ax, bot)
--- robot.py
+++ robot.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import List
 from abc import ABC, abstractmethod
-from matplotlib.widgets import Slider
 
 
 """This is a simplistic environment used to visualize the calculated robot's positions based on matplotlib."""
@@ -215,61 +214,6 @@ class Hexapod:
             self._bodyparts["legs"][leg].update_joints_position(positions[i])
 
 
-class ForwardKinematicsPreview:
-
-    def __init__(self, ax_:plt.Axes, robot: Hexapod):
-        self.robot = robot
-        self.ax = ax_
-        self.ax.set_title("Forward kinematics preview")
-
-        ax_leg_ang = plt.axes([0.25, 0.1, 0.65, 0.03])
-        ax_fem_ang = plt.axes([0.25, 0.15, 0.65, 0.03])
-        ax_tib_ang = plt.axes([0.25, 0.2, 0.65, 0.03])
-
-        self.leg_ang = Slider(ax=ax_leg_ang,
-                              label="Leg base angle",
-                              valmin=-90,
-                              valmax=90,
-                              valinit=0,
-                              orientation="horizontal")
-
-        self.fem_ang = Slider(ax=ax_fem_ang,
-                              label="Femur angle",
-                              valmin=-120,
-                              valmax=120,
-                              valinit=0,
-                              orientation="horizontal")
-
-        self.tib_ang = Slider(ax=ax_tib_ang,
-                              label="Tibia angle",
-                              valmin=-120,
-                              valmax=120,
-                              valinit=0,
-                              orientation="horizontal")
-
-        lim = 40
-        ax.set_xlim3d([-lim, lim])
-        ax.set_ylim3d([-lim, lim])  # todo: Automate adjusting the limits, since set_aspect does not work on 3d axes.
-        ax.set_zlim3d([-lim, lim])
-
-        self.leg_ang.on_changed(self.update)
-        self.fem_ang.on_changed(self.update)
-        self.tib_ang.on_changed(self.update)
-
-        self.robot.draw(self.ax)
-
-        plt.show()
-
-    def update(self, _):
-
-        angles = [[(ang * 60 + self.leg_ang.val) / 180 * np.pi,
-                   self.fem_ang.val / 180 * np.pi,
-                   self.tib_ang.val / 180 * np.pi] for ang in range(6)]
-
-        robot.update_leg_positions(angles)
-        robot.draw(ax)
-
-
 if __name__ == '__main__':
 
     fig = plt.figure(figsize=(8, 12))
@@ -284,4 +228,6 @@ if __name__ == '__main__':
     robot.add_leg(15, 20)
     robot.add_leg(15, 20)
 
-    ForwardKinematicsPreview(ax, robot)
+    robot.draw(ax)
+
+    plt.show()
